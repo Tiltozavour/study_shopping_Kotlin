834a11bc55f2667c5b4b5bdc1784c483b350abcd
Tiltozavour <soroka144@gmail.com>
2023-12-19 13:38:34 +0300

    Data

---
+++ app/src/main/java/com/example/study_shopping_kotlin/data/ShopListRepositoryImp.kt
@@ -0,0 +1,42 @@
+package com.example.study_shopping_kotlin.data
+
+import com.example.study_shopping_kotlin.domain.ShopListRepository
+import com.example.study_shopping_kotlin.domain.shop_item
+
+object  ShopListRepositoryImp:ShopListRepository {
+
+    private val shopList = mutableListOf<shop_item>()
+
+    private var autoIncrementId = 0
+
+
+    override fun addShopItemUseCase(shopItem: shop_item) {
+        if (shopItem.id == shop_item.UNDENTIFIED_ID){
+            shopItem.id = autoIncrementId++
+        }
+        shopList.add(shopItem)
+    }
+
+    override fun deleteShopItemUseCase(shopItem: shop_item) {
+        shopList.remove(shopItem)
+
+    }
+
+    override fun editShopItemUseCase(shopItem: shop_item) {
+        val oldElement = getShopItemUseCase(shopItem.id)
+        shopList.remove(oldElement)
+        addShopItemUseCase(shopItem)
+    }
+
+    override fun getShopItemUseCase(shopItemId: Int): shop_item {
+        return  shopList.find {
+            it.id==shopItemId
+        } ?: throw RuntimeException("Element with id $shopItemId not found}")
+    }
+
+    override fun getShopListUseCase(): List<shop_item> {
+        return shopList.toList()
+    }
+
+
+}
--- app/src/main/java/com/example/study_shopping_kotlin/domain/shop_item.kt
+++ app/src/main/java/com/example/study_shopping_kotlin/domain/shop_item.kt
@@ -4,9 +4,12 @@ data class shop_item(
     val name: String,
     val count: Int,
     val enabled: Boolean,
-    val id:Int,
+    var id:Int = UNDENTIFIED_ID,
 )
-/*companion object {
-    const val UNDENTIFIED_ID = -1
-}*/
+{
+    companion object {
+        const val UNDENTIFIED_ID = -1
+    }
+
+}
 
